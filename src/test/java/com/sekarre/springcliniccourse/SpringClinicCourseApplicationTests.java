6f7e5eb7568efa8ea9e82c6498217dd5f308bd02
Sekarre <zimny2121@gmail.com>
2020-09-01 19:14:37 +0200

    + more stuff

---
+++ src/test/java/com/sekarre/springcliniccourse/SpringClinicCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekarre.springcliniccourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClinicCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
