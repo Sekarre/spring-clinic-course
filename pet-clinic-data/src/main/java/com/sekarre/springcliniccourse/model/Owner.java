b75d0fb9f5b61189153632d1ac2c0514c66904db
Sekarre <zimny2121@gmail.com>
2020-09-02 22:46:38 +0200

    Project first refactor

---
+++ pet-clinic-data/src/main/java/com/sekarre/springcliniccourse/model/Owner.java
@@ -0,0 +1,4 @@
+package com.sekarre.springcliniccourse.model;
+
+public class Owner extends Person{
+}
---
+++ pet-clinic-data/src/main/java/com/sekarre/springcliniccourse/model/Vet.java
@@ -0,0 +1,4 @@
+package com.sekarre.springcliniccourse.model;
+
+public class Vet extends Person{
+}
