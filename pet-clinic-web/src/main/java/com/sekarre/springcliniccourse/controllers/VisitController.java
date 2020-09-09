3b35032b1313a15bce1d1364a9d0ebe9546f9b72
Sekarre <zimny2121@gmail.com>
2020-09-09 21:33:39 +0200

    Added vets JSON and bug fixes

--- pet-clinic-web/src/main/java/com/sekarre/springcliniccourse/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/com/sekarre/springcliniccourse/controllers/VisitController.java
@@ -11,7 +11,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 
 @Controller
 public class VisitController {
@@ -26,8 +27,15 @@ public class VisitController {
 
 
     @InitBinder
-    public void setAllowedFields(WebDataBinder dataBinder) {
+    public void dataBinder(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport(){
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
     }
 
     /**
